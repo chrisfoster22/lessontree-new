5dc61f985902c2c1d8d949b60a6a225b8b6f2bfa
Chris Foster <fosterchris22@gmail.com>
2015-03-17 12:07:45 -0400

    Add method for viewing grade levels

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ActiveRecord::Base
   has_many :plans
   has_many :lessons
+  has_many :grade_levels
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+
+  def grade
+    grade_id = self.grade_level_id
+    grade = GradeLevel.find_by_id(grade_id)
+    grade.title
+  end
 end
