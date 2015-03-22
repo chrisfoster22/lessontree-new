e848d99b7433119561edd06d3379d4152b5aaf40
amplourde <amplourde@gmail.com>
2015-03-22 13:44:46 -0400

    committing to pull

--- db/schema.rb
+++ db/schema.rb
@@ -76,14 +76,6 @@ ActiveRecord::Schema.define(version: 20150320180517) do
     t.string   "searchable_type"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-
-  create_table "mercury_images", force: :cascade do |t|
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "plans", force: :cascade do |t|
