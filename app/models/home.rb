443cdd1f65378d806ca28dfa62dea6685f4faf1f
Parker Ennis <rpennis@ncsu.edu>
2015-03-22 16:15:45 -0400

    head issue

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,19 +1,12 @@
 class Home < ActiveRecord::Base
   has_many :lessons
   has_many :documents
-<<<<<<< HEAD
-=======
-  # include PgSearch
-  multisearchable :against => [:topic, :description, :content, :title]
->>>>>>> f5eb947f354898324361ce8f9322e37bf6c43165
 
-  # searchable do
-  #   text :topic, :boost => 5
-  #   text :description, :created_at
-  #   text :documents do
-  #     documents.map(&:description)
-  #   end
-  #   time :created_at
-  #   string :month_created
-  # end
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
