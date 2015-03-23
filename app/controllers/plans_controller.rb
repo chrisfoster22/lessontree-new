9f85721991f5a0d0ec3e22d8b3bdb6820f16bf30
Chris Foster <fosterchris22@gmail.com>
2015-03-23 12:26:41 -0400

    Starred lessons and plans show up.

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,13 @@
 class PlansController < ApplicationController
   attr_accessor :title, :description
   def index
-    @plans = Plan.find_by(current_user.id).order("created_at DESC")
+    @plans = Plan.where(user_id: current_user.id).order("created_at DESC")
+    stars = Star.where(user_id: current_user.id, lesson_id: nil)
+    @starred_plans = []
+    stars.each do |s|
+      plan = Plan.find_by(id: s.plan_id)
+      @starred_plans << plan
+    end
   end
 
   def show
