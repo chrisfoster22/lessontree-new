ca13beebe2ec9ca6e59ca168b408e18ec41b3be9
Chris Foster <fosterchris22@gmail.com>
2015-03-23 11:36:27 -0400

    Add lessons to plans.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,6 +7,10 @@ class LessonsController < ApplicationController
     @lesson = Lesson.find_by_id(params[:id])
     @star = Star.new
     @documents = @lesson.documents
+    @plan = @lesson.plan
+    if @plan
+      @default = @plan.id
+    end 
   end
 
   def new
@@ -40,7 +44,7 @@ class LessonsController < ApplicationController
 private
 
   def lesson_params
-    params.require(:lesson).permit(:upload, :topic, :description, :id, :user_id)
+    params.require(:lesson).permit(:upload, :topic, :description, :id, :user_id, :plan_id)
   end
 
 end
