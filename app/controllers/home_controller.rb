7d568bb45d0aca633b1355eda4ff6a86602a5f02
Parker Ennis <rpennis@ncsu.edu>
2015-03-24 19:56:25 -0400

    search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,13 @@ class HomeController < ApplicationController
 
   def index
     if params[:search]
-      @lessons = Lesson.all
+      @lessons = Lesson.search(params[:search]).order(:updated_at).reverse.first(9)
+    else
+      @lessons = Lesson.all.order(:updated_at).reverse.first(9)
+    end
+  end
+end
       # filtered_lessons = []
       # lessons = Lesson.search(params[:search])
       # lessons = filtered_lessons.each {|l| lessons << l if l.subjects.include?(params[:subject_id])}
       # @lessons = lessons.map(&:title)
-    else
-      @lessons = Lesson.all.order(:updated_at).reverse.first(12)
-    end
-  end
-
-end
