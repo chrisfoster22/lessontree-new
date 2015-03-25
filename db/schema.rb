22eab42d0acbf3f4f4418633db59c0d6325c6962
Parker Ennis <rpennis@ncsu.edu>
2015-03-25 13:49:05 -0400

    devise oauth

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150325154116) do
-ActiveRecord::Schema.define(version: 20150324184237) do
-ActiveRecord::Schema.define(version: 20150325152743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +70,7 @@ ActiveRecord::Schema.define(version: 20150325152743) do
     t.string   "topic"
     t.text     "description"
     t.integer  "user_id"
+    t.integer  "plan_id"
     t.integer  "star_count"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
@@ -93,6 +92,7 @@ ActiveRecord::Schema.define(version: 20150325152743) do
   create_table "stars", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "lesson_id"
+    t.integer  "plan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
