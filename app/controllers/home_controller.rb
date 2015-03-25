ea3cbfef60823eed67f17fd384629ec52465a6d9
Chris Foster <fosterchris22@gmail.com>
2015-03-25 08:29:11 -0400

    Still working on search bar.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,9 @@ class HomeController < ApplicationController
 
   def index
     if params[:search]
-      @lessons = Lesson.all
+      # @lessons = Lesson.all
       # filtered_lessons = []
-      # lessons = Lesson.search(params[:search])
+      @lessons = Lesson.search(params[:search])
       # lessons = filtered_lessons.each {|l| lessons << l if l.subjects.include?(params[:subject_id])}
       # @lessons = lessons.map(&:title)
     else
