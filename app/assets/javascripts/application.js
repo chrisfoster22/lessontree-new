6eaf4987443204c03323768b7b6d1e3a29f12396
amplourde <amplourde@gmail.com>
2015-03-25 15:32:08 -0400

    Adding advanced search dropdown section to style guide

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -77,3 +77,12 @@ $(document).ready(function () {
       }
     });
   });
+
+  $(document).ready(function() {
+  var expanderTrigger = document.getElementById("js-expander-trigger");
+  var expanderContent = document.getElementById("js-expander-content");
+
+  $('#js-expander-trigger').click(function(){
+    $(this).toggleClass("expander-hidden");
+  });
+});
