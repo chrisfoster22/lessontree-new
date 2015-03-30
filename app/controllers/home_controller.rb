78b267af98121aea1b3741376a95324315f60aa2
Chris Foster <fosterchris22@gmail.com>
2015-03-29 20:58:44 -0400

    Change

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @lessons = Lesson.query(params).page(params[:page]).per(5)
-    # @lessons = Lesson.paginate(page: params[:page],:per_page => 5).query(params)
     @grades = GradeLevel.all
     @subjects = Subject.all
   end
 end
-      # filtered_lessons = []
-      # lessons = Lesson.search(params[:search])
-      # lessons = filtered_lessons.each {|l| lessons << l if l.subjects.include?(params[:subject_id])}
-      # @lessons = lessons.map(&:title)
