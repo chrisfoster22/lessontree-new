f4f969c7360b4358089be470cec0cba6a95cca7c
Chris Foster <fosterchris22@gmail.com>
2015-03-30 11:18:49 -0400

    authorization on documents. start working on lesson show modal

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -22,9 +22,7 @@ class Lesson < ActiveRecord::Base
                 .where('grade_levels.id = ?', params[:grade_level_id])
                 .where('topic ILIKE ? OR description ILIKE ?',
                        "%#{params[:search]}%", "%#{params[:search]}%")
-      # lessons.paginate :per_page => 5,
-      #                  :page => page,
-      #                  :order => 'updated_at DESC'
+      lessons.uniq
     else
       Lesson.all
     end
