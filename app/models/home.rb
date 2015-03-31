116ff2c4738b9d30c24f7957e927751bfcb0d46c
Chris Foster <fosterchris22@gmail.com>
2015-03-31 09:20:04 -0400

    Documents show up on lesson show page.

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,12 +1,4 @@
 class Home < ActiveRecord::Base
   has_many :lessons
   has_many :documents
-
-
-
-  # def self.search(search)
-  #   search_condition = "%" + search + "%"
-  #   find(:all, :conditions => ['title LIKE ? OR description LIKE ?', search_condition, search_condition])
-  # end
-
 end
